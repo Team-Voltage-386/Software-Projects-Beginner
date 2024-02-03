f326fcdd43674e74b3b96950879a6193c8b1d50b
Jake <114447363+V0KAL@users.noreply.github.com>
2024-02-02 20:40:23 -0500

    Updated Sequential

--- LEDDemoboardExample-Imported/src/main/java/frc/robot/DiscoModeHandler/DiscoCollective.java
+++ LEDDemoboardExample-Imported/src/main/java/frc/robot/DiscoModeHandler/DiscoCollective.java
@@ -1,5 +1,59 @@
 package frc.robot.DiscoModeHandler;
+import frc.robot.subsystems.LightSubsystem;
+import edu.wpi.first.wpilibj.Timer;
 
 public class DiscoCollective {
-    
+
+    DiscoModeState modeState;
+    DiscoLightState lightState;
+    Timer timer;
+
+    private DiscoCollective(){ //Contstructor
+        Timer timer = new Timer();
+        timer.start();
+    }
+
+    public void discoCollective(DiscoModeState modeState,DiscoLightState lightState, LightSubsystem m_LightSubsystem){
+        //read modeState
+        if (modeState.get() == ModeState.COLLECTIVE){
+            //read lightState
+            switch (lightState.get()){
+                //do something about it
+                case INIT: {
+                    m_LightSubsystem.changeAllLEDColor(255, 255, 255).schedule();
+                    if (timer.get() > 0.1){
+                        timer.reset();
+                        lightState.set(LightState.RED);
+                        break;
+                    }
+                }
+                case RED: {
+                    m_LightSubsystem.changeAllLEDColor(255, 0, 0).schedule();
+                    if (timer.get() >0.1){
+                        timer.reset();
+                        lightState.set(LightState.GREEN);
+                        break;
+                    }
+                }
+                case GREEN:{
+                    m_LightSubsystem.changeAllLEDColor(0, 255, 0).schedule();
+                    if (timer.get() > 0.1){
+                        timer.reset();
+
+                        break;
+                    }
+                }
+                case BLUE:{
+                    m_LightSubsystem.changeAllLEDColor(0, 0, 255).schedule();
+                    if (){
+
+                    }
+
+                }
+
+            }
+            
+
+        }
+    }
 }
--- LEDDemoboardExample-Imported/src/main/java/frc/robot/DiscoModeHandler/DiscoModeOrganizer.java
+++ LEDDemoboardExample-Imported/src/main/java/frc/robot/DiscoModeHandler/DiscoModeOrganizer.java
@@ -1,54 +1,36 @@
 package frc.robot.DiscoModeHandler;
 
-import edu.wpi.first.wpilibj2.command.Command;
 
-public class DiscoModeOrganizer extends Command { //Decides what to do
+public class DiscoModeOrganizer { //Decides what to do
 
     
     DiscoModeState modeState;
-    DiscoLightState lightState;
 
     public DiscoModeOrganizer(){
-        this.modeState = new DiscoModeState(ModeState.INIT);
-        this.lightState = new DiscoLightState(LightState.INIT);
+        this.modeState = new DiscoModeState(ModeState.COLLECTIVE);
     }
 
-    public void setStates(int switchState /* whether to use Sequential (1) or Collective (2), provide 0 for Initial */, 
-    int switchLights/* Which color to switch to, 0 is White (Init), 1 is Red, 2 is Blue, 3 is Green */){
-        switch (switchState){
-            case 0: {
-                modeState.set(ModeState.INIT);
-                break;
-            }
-            case 1: {
-                modeState.set(ModeState.SEQUENTIAL);
-                break;
-            }
-            case 2: {
-                modeState.set(ModeState.COLLECTIVE);
-                break;
-            }
-            default: {
-                modeState.set(ModeState.INIT);
-                break;
-            }
-        }
-        switch (switchLights){
-            case 0: {
-                lightState.set(LightState.INIT);
-                break;
-            }
-            case 1: {
-                lightState.set(LightState.RED);
-                break;
-            }
-            case 2:{
-                lightState.set(LightState.GREEN);
-                break;
-            }
-            case 3: {
-                lightState.set(LightState.BLUE);
-                break;
+    public void setMode(int switchMode /* whether to use Collective (0) Sequential (1) or Rainbow (2) */){
+        if ((switchMode > 2) || (switchMode < 0)){
+            System.out.println("Please provide a whole integer between ");
+        } else {
+            switch (switchMode){
+                case 0: {
+                    modeState.set(ModeState.COLLECTIVE);
+                    break;
+                }
+                case 1: {
+                        modeState.set(ModeState.SEQUENTIAL);
+                    break;
+                }
+                case 2: {
+                    modeState.set(ModeState.RAINBOW);
+                    break;
+                }
+                default: {
+                    modeState.set(ModeState.COLLECTIVE);
+                    break;
+                }
             }
         }
         return;
