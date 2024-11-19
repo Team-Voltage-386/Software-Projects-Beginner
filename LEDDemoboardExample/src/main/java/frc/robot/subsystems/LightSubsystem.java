6af49be39b9abf64067276a53c5e47ff8eb59a73
Rodrigo Betances <122638585+RaidZeroSix@users.noreply.github.com>
2024-11-18 22:22:29 -0500

    No more commands in LightSubsystem.java

--- LEDDemoboardExample/src/main/java/frc/robot/subsystems/LightSubsystem.java
+++ LEDDemoboardExample/src/main/java/frc/robot/subsystems/LightSubsystem.java
@@ -7,7 +7,6 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.util.Color;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 //import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
@@ -41,6 +40,12 @@ public class LightSubsystem extends SubsystemBase {
         return;
     }
 
+    public void changeAllLEDColor(int r, int g, int b) {
+        for (int i = 0; i < ledBuffer.getLength(); i++) {
+            setToColor(i, r, g, b);
+        }
+    }
+
     public boolean areLightsOn() {
         // Lights are on if at least one LED has either red, green, or blue that is not
         // 0.
@@ -52,27 +57,6 @@ public class LightSubsystem extends SubsystemBase {
             lightsAreOn |= c.red > 0.1;
         }
         return lightsAreOn;
-    }
-
-    public void changeAllLEDColor(int r, int g, int b) {
-        for (int i = 0; i < ledBuffer.getLength(); i++) {
-            setToColor(i, r, g, b);
-        }
-    }
-
-    public Command setAllBlue() {
-        return runOnce(
-                () -> {
-                    for (int i = 0; i < ledBuffer.getLength(); i++) {
-                        ledBuffer.setRGB(i, 0, 0, 255);
-                    }
-                });
-    }
-
-    public void allPurple() {
-        for (int i = 0; i < ledBuffer.getLength(); i++) {
-            ledBuffer.setRGB(i, 100, 0, 200);
-        }
     }
 
     /**
